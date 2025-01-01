a4305b50dac67dce5cd332e17c797fa973787714
KaitoMuraoka <kaitoboshi@icloud.com>
2025-01-01 22:42:09 +0900

    close #4

--- index.js
+++ index.js
@@ -3,8 +3,30 @@
 import { program } from 'commander';
 import { checkoutCommand } from './commands/checkout.js';
 import { deleteCommand } from './commands/delete.js';
+import { execSync } from 'child_process';
 
-program.addCommand(checkoutCommand);
-program.addCommand(deleteCommand);
+async function loadCustomCommand() {
+  try {
+      const customCheckout = execSync('git config --get git-branch-kit.checkout', { encoding: 'utf8' }).trim();
+      if (customCheckout === 'checkout') {
+          program.addCommand(checkoutCommand);
+      }
+  } catch (error) {
+      console.warn('Custom checkout command is not set in git config. Using default.');
+      program.addCommand(checkoutCommand);
+  }
 
+  try {
+      const customDelete = execSync('git config --get git-branch-kit.delete', { encoding: 'utf8' }).trim();
+      if (customDelete === 'delete') {
+          program.addCommand(deleteCommand);
+      }
+  } catch (error) {
+      console.warn('Custom delete command is not set in git config. Using default.');
+      program.addCommand(deleteCommand);
+  }
+}
+
+
+await loadCustomCommand();
 program.parse(process.argv);
