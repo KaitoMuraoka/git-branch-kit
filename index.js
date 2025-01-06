a76fc399b471bfed93776d999dffcf5c24e071d2
KaitoMuraoka <kaitoboshi@icloud.com>
2025-01-06 12:26:20 +0900

    put back command

--- index.js
+++ index.js
@@ -3,42 +3,17 @@
 import { program } from 'commander';
 import { checkoutCommand } from './commands/checkout.js';
 import { deleteCommand } from './commands/delete.js';
-import { execSync } from 'child_process';
-import { readFileSync } from 'fs';
-import path from 'path';
-
-async function loadCustomCommand() {
-  try {
-    const customCheckout = execSync('git config --get git-branch-kit.checkout', { encoding: 'utf8' }).trim();
-    if (customCheckout === 'checkout') {
-      program.addCommand(checkoutCommand);
-    }
-  } catch (error) {
-    console.warn('Custom checkout command is not set in git config. Using default.');
-    program.addCommand(checkoutCommand);
-  }
-
-  try {
-    const customDelete = execSync('git config --get git-branch-kit.delete', { encoding: 'utf8' }).trim();
-    if (customDelete === 'delete') {
-      program.addCommand(deleteCommand);
-    }
-  } catch (error) {
-    console.warn('Custom delete command is not set in git config. Using default.');
-    program.addCommand(deleteCommand);
-  }
-}
 
 function showVersion() {
-  const packageJsonPath = path.resolve(process.cwd(), './package.json');
-  const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf8'));
-
+  const versionNumner = "v1.1.0";
   program
-    .name('git-branch-kit')
-    .description('A CLI tool for easier git branch operations.')
-    .version(packageJson.version, '-v, --version', 'Output the current version of git-branch-kit');
+  .name('string-util')
+  .description('CLI to some JavaScript string utilities')
+  .version(versionNumner);
 }
 
 showVersion();
-await loadCustomCommand();
+program.addCommand(checkoutCommand);
+program.addCommand(deleteCommand);
+
 program.parse(process.argv);
